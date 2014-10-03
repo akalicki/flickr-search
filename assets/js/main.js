700162206c86b5a35b76125eeb6d82c08dad4e42
Alex Kalicki <alexander.kalicki@gmail.com>
2014-10-03 00:26:44 -0400

    get basic search working

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,10 +23,14 @@ function buttonInit(flickr) {
 
 /* SEARCH METHODS */
 
+var curOptions = {};
+var curPages = 0;
+
 function basicSearch(flickr) {
     var options = {
         text: $('#basicText').val(),
-        per_page: 10
+        per_page: 10,
+        page: 1
     };
 
     imageSearch(flickr, options);
@@ -38,7 +42,8 @@ function advancedSearch(flickr) {
         min_upload_date: $('#advUploadSince').val() !== '' ? Date.parse($('#advUploadSince').val()) / 1000 : '',
         max_upload_date: $('#advUploadUntil').val() !== '' ? Date.parse($('#advUploadUntil').val()) / 1000 : '',
         tags: $('#advTags').val(),
-        per_page: 10
+        per_page: 10,
+        page: 1
     };
 
     addUserAndSearch(flickr, options);
@@ -62,15 +67,37 @@ function addUserAndSearch(flickr, options) {
 }
 
 function imageSearch(flickr, options) {
-    closeModal();
-
     flickr.photos.search(options, function(err, result) {
         if (err) {
             handleError(err);
         } else {
-            console.log(result);
+            closeModal();
+            curOptions = options;
+            curPages = result.photos.pages;
+            showPhotos(result.photos.photo);
         }
     });
+}
+
+/* DISPLAY METHODS */
+
+function showPhotos(photos) {
+    var photo_url,
+        photo_src,
+        p,
+        thumb_clone;
+
+    $('#imgGallery').children().remove();
+    for (i = 0; i < photos.length; i++) {
+        p = photos[i];
+        photo_src = 'https://farm' + p.farm + '.staticflickr.com/' + p.server + '/' + p.id + '_' + p.secret + '_n.jpg';
+        photo_url = 'https://www.flickr.com/photos/' + p.owner + '/' + p.id;
+
+        thumb_clone = $('#imgDummy').children('.img-thumbnail').first().clone();
+        thumb_clone.children('a').first().attr('href', photo_url);
+        thumb_clone.find('img').first().attr({ src: photo_src, alt: p.title });
+        thumb_clone.appendTo('#imgGallery');
+    }
 }
 
 /* HELPER METHODS */
